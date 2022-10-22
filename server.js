4208da7d7d1a2fb671b99339ff58713e02c35ea8
雞ry牛羊雞ry牛羊 <kahung930809@gmail.com>
2022-10-21 22:15:28 -0600

    updating database

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-slippery-76335',
       user : 'postgres',
-      password : 'test',
+      password : '',
       database : 'smart-brain'
     }
   });
